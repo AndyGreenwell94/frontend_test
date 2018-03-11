c2aad915e5e470e68adea60bb11db173759e4f55
Andrew Potekhin <andrew.potekhin94@gmail.com>
2018-03-11 12:06:22 +0700

    Add routing module

--- src/main.js
+++ src/main.js
@@ -1,8 +1,20 @@
 import Vue from 'vue'
+import BootstrapVue from 'bootstrap-vue'
+import Vuex from 'vuex'
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+import router from './router'
+
 import App from './App.vue'
+
+Vue.use(BootstrapVue)
+Vue.use(Vuex)
 
 Vue.config.productionTip = false
 
 new Vue({
+  router,
   render: h => h(App)
 }).$mount('#app')
