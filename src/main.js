df599ac5ded1a9ce412fe9043fd15259f6faec9e
Andrew Potekhin <andrew.potekhin94@gmail.com>
2018-03-11 12:25:00 +0700

    Add vuex store

--- src/main.js
+++ src/main.js
@@ -1,20 +1,20 @@
 import Vue from 'vue'
 import BootstrapVue from 'bootstrap-vue'
-import Vuex from 'vuex'
 
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 import router from './router'
+import store from './store'
 
 import App from './App.vue'
 
 Vue.use(BootstrapVue)
-Vue.use(Vuex)
 
 Vue.config.productionTip = false
 
 new Vue({
   router,
+  store,
   render: h => h(App)
 }).$mount('#app')
